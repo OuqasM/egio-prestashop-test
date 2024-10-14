2efd51792582c965b7142b8e39bf42be74bf7474
OuqasM <ouqas.pro@gmail.com>
2024-10-14 23:49:35 +0100

    Initiate Service with Guzzle Client & implement configuration form(incomlet  work)

--- ps_weather_forecast.php
+++ ps_weather_forecast.php
@@ -30,4 +30,35 @@ class Ps_Weather_Forecast extends Module
     {
         return parent::uninstall() && Configuration::deleteByName('PS_WEATHER_API_KEY');
     }
+
+    public function getContent()
+    {
+        if (Tools::isSubmit('submitPsWeatherForecast')) {
+            $apiKey = Tools::getValue('PS_WEATHER_API_KEY');
+            Configuration::updateValue('PS_WEATHER_API_KEY', $apiKey); // updateOrCreate value on DB
+        }
+        
+        // render config form
+        return $this->renderForm();
+    }
+
+    protected function renderForm()
+    {
+        $helper = new HelperForm();
+        $helper->submit_action = 'submitPsWeatherForecast';
+        $helper->fields_value['PS_WEATHER_API_KEY'] = Configuration::get('PS_WEATHER_API_KEY');
+
+        return $helper->generateForm([[
+            'form' => [
+                'legend' => ['title' => $this->l('Settings')],
+                'input' => [[
+                    'type' => 'text',
+                    'label' => $this->l('Meteo-Concept API Key'),
+                    'name' => 'PS_WEATHER_API_KEY',
+                    'required' => true,
+                ]],
+                'submit' => ['title' => $this->l('Save')],
+            ]
+        ]]);
+    }
 }
---
+++ src/Service/WeatherApiService.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace PsWeatherForecast\Service;
+
+use GuzzleHttp\Client;
+
+class WeatherApiService
+{
+    private $client;
+    private $apiKey;
+
+    public function __construct()
+    {
+        $this->client = new Client(['base_uri' => 'https://api.meteo-concept.com/api/']);
+        $this->apiKey = \Configuration::get('PS_WEATHER_API_KEY');
+    }
+
+    public function getCurrentWeather($city)
+    {
+        return $this->makeRequest('forecast/daily', $city);
+    }
+
+    public function getForecastByDay($city)
+    {
+        return $this->makeRequest('forecast/daily', $city);
+    }
+
+    public function getForecastByQuarterDay($city)
+    {
+        return $this->makeRequest('forecast/nextHours', $city);
+    }
+
+    public function getHourlyForecast($city)
+    {
+        return $this->makeRequest('forecast/hourly', $city);
+    }
+
+    private function makeRequest($endpoint, $city)
+    {
+        try {
+            $response = $this->client->request('GET', $endpoint, [
+                'query' => [
+                    'token' => $this->apiKey,
+                    'insee' => $this->getInseeCode($city)
+                ]
+            ]);
+
+            return json_decode($response->getBody()->getContents(), true);
+        } catch (\Exception $e) {
+            $errorMessage = "";
+
+            switch ($e->getCode()) {
+                case 400:
+                    $errorMessage = "Il manque un paramètre ou la valeur est incorrecte";
+                    break;
+                case 401:
+                    $errorMessage = "Vous n'êtes pas authentifié(e)";
+                    break;
+                case 403:
+                    $errorMessage = "Vous n'avez pas accès à cette page";
+                    break;
+                case 404:
+                    $errorMessage = "La page demandée n'existe pas";
+                    break;
+                case 500:
+                    $errorMessage = "Une erreur est survenue, merci de réessayer plus tard";
+                    break;
+                case 503:
+                    $errorMessage = "L'API est momentanément indisponible, merci de réessayer dans quelques minutes";
+                    break;
+                default:
+                    $errorMessage = "Impossible de récupérer les données météorologiques.";
+                    break;
+            }
+
+            return ['error' => $errorMessage];
+        }
+    }
+
+    private function getInseeCode($city)
+    {
+        // todo: mustfind a solution for this  
+    }
+}
